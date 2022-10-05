da50de0bf4adbd77c9a4c75e4f2cb1d01eacaa30
hayeongKo <polarpheno@icloud.com>
2022-10-06 00:51:50 +0900

    week3 standard mission clear

---
+++ week3_standard/week3_standard/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  week3_standard
+//
+//  Created by 고하영 on 2022/10/06.
+//
+import UIKit
+
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
